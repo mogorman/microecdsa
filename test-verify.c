a90199499eccaad82e296b10fbdba8babdc280ef
mog <mog@rush.rldn.net>
2013-09-09 14:18:42 -0500

    testing things

--- test-verify.c
+++ test-verify.c
@@ -48,105 +48,112 @@ uint16_t challenge();
 int main()
 {
   uint16_t my_challenge = 0;
-  uint32_t time_stamp = time(NULL), temp, sig_len;
+  uint32_t time_stamp, temp, sig_len;
   uint8_t sig[70], msg[6];
-  uint8_t pub_x[35] = {0};
-  uint8_t pub_y[35] = {0};
+  uint8_t pub_x[34] = {0};
+  uint8_t pub_y[34] = {0};
   uint8_t hash[32] = {0};
 
   uint8_t msg_len;
-  int i = 0;
+  int i = 0, k = 0;
+//good key
 //  uint8_t private_key[32] = { 0x42,0xe1,0x82,0x9b,0x74,0x42,0x34,0x79,0xc6,0xbc,0xfa,0x54,
 //			      0x09,0xe9,0x35,0x93,0xe9,0xed,0x9c,0xde,0x5a,0x87,0xe7,0x08,
 //			      0xc6,0x07,0x49,0x73,0xe9,0x04,0x27,0x28 };
+//bad key
   uint8_t private_key[32] = { 0x37,0xb0,0x6b,0xdd,0xf1,0x31,0x5e,0x3a,0xc5,0x50,0x1b,0xac,
 			      0xb8,0x85,0x2f,0xc5,0xb8,0x2a,0xe0,0x25,0x02,0xb2,0xdb,0x41,
 			      0xa9,0xf6,0x17,0xd1,0x5b,0x4a,0xb2,0x3b };
+  //uint8_t private_key[32] = {0};
 
   init_rand();
-  my_challenge = challenge();
-  temp = time_stamp;
-  msg[0] = temp >> 24;
-  msg[1] = temp >> 16;
-  msg[2] = temp >> 8;
-  msg[3] = temp;
-  temp = my_challenge;
-  msg[4] = temp >> 8;
-  msg[5] = temp; 
-  msg_len = 6;
 
+  while(1) {
+    k++;
+    time_stamp = time(NULL);
+    my_challenge = challenge();
+    temp = time_stamp;
+    msg[0] = temp >> 24;
+    msg[1] = temp >> 16;
+    msg[2] = temp >> 8;
+    msg[3] = temp;
+    temp = my_challenge;
+    msg[4] = temp >> 8;
+    msg[5] = temp; 
+    msg_len = 6;
+    //for(i = 0; i < 32; i++) {
+    //  private_key[i] = random32() &0xFF;
+    //}
+    ecdsa_sign(private_key, msg, msg_len, sig, &sig_len);
+    ecdsa_pubkey(private_key, &pub_x, &pub_y);
 
-  ecdsa_sign(private_key, msg, msg_len, sig, &sig_len);
+    if(!(verify_user(msg, sig, sig_len, pub_x, hash))) {
+      printf("succesful verifications %d\n", k);
+      continue;
+    } else {
+      printf("something failed! %d\n", k);
+    }
 
-  ecdsa_pubkey(private_key, &pub_x, &pub_y);
-
-
-  if(!(verify_user(msg, sig, sig_len, pub_x, hash))) {
-      printf("succesful verification\n");
-  } else {
-      printf("something failed!\n");
-  }
-
-  printf("time:\t%d\n", time_stamp);
-  printf("chal:\t%d\n", my_challenge);
-  printf("msg:\t");
-  for (i = 0; i < msg_len; i++) {
-    if(msg[i] < 0x10) {
-      printf("0");
+    printf("time:\t%d\n", time_stamp);
+    printf("chal:\t%d\n", my_challenge);
+    printf("msg:\t");
+    for (i = 0; i < msg_len; i++) {
+      if(msg[i] < 0x10) {
+	printf("0");
+      }
+      printf("%X ", msg[i]);
     }
-    printf("%X ", msg[i]);
-  }
-  printf("\n");
-  printf("pub:\t03 ");
-  for (i = 2; i < 34; i++) {
-    if(pub_x[i] < 0x10) {
-      printf("0");
+    printf("\n");
+    printf("pub:\t03 ");
+    for (i = 2; i < 34; i++) {
+      if(pub_x[i] < 0x10) {
+	printf("0");
+      }
+      printf("%X ", pub_x[i]);
     }
-    printf("%X ", pub_x[i]);
-  }
-  printf("\n");
-  printf("priv:\t");
-  for (i = 0; i < 32; i++) {
-    if(private_key[i] < 0x10) {
-      printf("0");
+    printf("\n");
+    printf("priv:\t");
+    for (i = 0; i < 32; i++) {
+      if(private_key[i] < 0x10) {
+	printf("0");
+      }
+      printf("%X ", private_key[i]);
     }
-    printf("%X ", private_key[i]);
-  }
-  printf("\n");
-  printf("pub_x:\t");
-  for (i = 2; i < 34; i++) {
-    if(pub_x[i] < 0x10) {
-      printf("0");
+    printf("\n");
+    printf("pub_x:\t");
+    for (i = 0; i < 34; i++) {
+      if(pub_x[i] < 0x10) {
+	printf("0");
+      }
+      printf("%X ", pub_x[i]);
     }
-    printf("%X ", pub_x[i]);
-  }
-  printf("\n");
-  printf("pub_y:\t");
-  for (i = 2; i < 34; i++) {
-    if(pub_y[i] < 0x10) {
-      printf("0");
+    printf("\n");
+    printf("pub_y:\t");
+    for (i = 0; i < 34; i++) {
+      if(pub_y[i] < 0x10) {
+	printf("0");
+      }
+      printf("%X ", pub_y[i]);
     }
-    printf("%X ", pub_y[i]);
-  }
-  printf("\n");
-  printf("hash:\t");
-  for (i = 0; i < 32; i++) {
-    if(hash[i] < 0x10) {
-      printf("0");
+    printf("\n");
+    printf("hash:\t");
+    for (i = 0; i < 32; i++) {
+      if(hash[i] < 0x10) {
+	printf("0");
+      }
+      printf("%X ", hash[i]);
     }
-    printf("%X ", hash[i]);
-  }
-  printf("\n");
-  printf("sig:\t");
-  for (i = 0; i < sig_len; i++) {
-    if(sig[i] < 0x10) {
-      printf("0");
+    printf("\n");
+    printf("sig:\t");
+    for (i = 0; i < sig_len; i++) {
+      if(sig[i] < 0x10) {
+	printf("0");
+      }
+      printf("%X ", sig[i]);
+      if(!((i+1)%32) && i !=0) printf("\n\t");
     }
-    printf("%X ", sig[i]);
-    if(!((i+1)%32) && i !=0) printf("\n\t");
+    printf("\n");
   }
-  printf("\n");
-
 
   return 0;
 }
